90cf65a81d38ea56d27106390876c22e394ee8bc
ROHIT RAWAT <rr287090@gmail.com>
2026-01-26 13:10:54 +0530

    Added brief about the VAR/LET  keyword

--- BasicsJavascript/01VariablesBasics.js
+++ BasicsJavascript/01VariablesBasics.js
@@ -22,3 +22,18 @@ let myAge = 25; this is called initialization (Means giving a variable a first v
 myAge = 24; this is called assignment ( means changing or giving the value to an existing variable)
 
 */
+
+// Using Var keyword
+
+/* Hinglish Explanation :
+
+
+    Var = Function Scoped Variable
+    Let = Block Scoped Variable
+    Const = Block Scoped Constant Variable
+
+    Function Scoped Variable means ki agar aapne var keyword ka use kiya hai to wo variable sirf usi function ke andar accessible hoga jaha pe wo declare hua hai.
+    Block Scoped Variable means ki agar aapne let ya const keyword ka use kiya hai to wo variable us block ke andar accessible hoga jaha pe wo declare hua hai. Block matlab curly braces {} ke andar wala area.
+    
+
+*/
